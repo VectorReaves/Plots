c7ab40848fa8b9dc8789889494e834fa44dda82c
VectorReaves <88208618+VectorReaves@users.noreply.github.com>
2021-08-11 19:58:04 -0400

    Add files via upload

---
+++ february.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Load data
+data = pd.read_csv('data/ufo_sightings.csv')
+
+# Plot
+plt.figure(figsize=(6.8, 4.4))
+x = range(len(data['Day']))
+plt.plot(x, data['February'])
+plt.xticks(x, data['Day'])
+plt.xlabel('Day')
+plt.ylabel('February')
+plt.show()
